bdfb1ee5a40f10b9198b00ef93680afd20f4bf0a
MohamedAlaa9758 <ma2612557@gmail.com>
2025-11-02 03:23:16 +0200

    2 commit

--- src/app/(pages)/api/get-cart/route.ts
+++ src/app/(pages)/api/get-cart/route.ts
@@ -1,20 +1,3 @@
-// import { getUserToken } from "@/components/Helpers/getUserToken";
-// import { CartResponse } from "@/interfaces";
-// import { NextResponse } from "next/server";
-
-// export async function GET() {
-//   const token = await getUserToken();
-
-//   const response = await fetch(`${process.env.URL_API}/cart`, {
-//     method: "GET",
-//     headers: {
-//       token: token + "",
-//     },
-//   });
-//   const data: CartResponse = await response.json();
-//   return NextResponse.json(data);
-// }
-
 import { getUserToken } from "@/components/Helpers/getUserToken";
 import { CartResponse } from "@/interfaces";
 import { NextResponse } from "next/server";
@@ -22,17 +5,34 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const token = await getUserToken();
 
-  if (!token) {
-    return NextResponse.json({ statusMsg: "fail", message: "No token found. Please login again." }, { status: 401 });
-  }
-
   const response = await fetch(`${process.env.URL_API}/cart`, {
     method: "GET",
     headers: {
-      Authorization: `Bearer ${token}`,
+      token: token + "",
     },
   });
-
   const data: CartResponse = await response.json();
   return NextResponse.json(data);
 }
+
+// import { getUserToken } from "@/components/Helpers/getUserToken";
+// import { CartResponse } from "@/interfaces";
+// import { NextResponse } from "next/server";
+
+// export async function GET() {
+//   const token = await getUserToken();
+
+//   if (!token) {
+//     return NextResponse.json({ statusMsg: "fail", message: "No token found. Please login again." }, { status: 401 });
+//   }
+
+//   const response = await fetch(`${process.env.URL_API}/cart`, {
+//     method: "GET",
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//     },
+//   });
+
+//   const data: CartResponse = await response.json();
+//   return NextResponse.json(data);
+// }
